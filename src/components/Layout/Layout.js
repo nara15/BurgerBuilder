e284c35c47eafa80b309106119b031c2354509c4
nara15 <marionaranjo12@gmail.com>
2020-07-18 22:49:16 -0600

    Adding toggle for mobile devices

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,39 @@
 
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Layout.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
-const layout = (props) => (
-    <>
-        <Toolbar/>
-        <SideDrawer />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </>
-);
+class Layout extends Component {
 
-export default layout;
+    state = {
+        showSideDrawer : false
+    }
+
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false})
+    }
+
+    sideDrawerOpenedHandler = () => {
+        this.setState((prevState) => {
+            return { showSideDrawer: !prevState.showSideDrawer }
+        });
+    }
+
+
+    render() {
+        return (
+            <>
+                <Toolbar drawerToggleClicked={this.sideDrawerOpenedHandler}/>
+                <SideDrawer 
+                    open={this.state.showSideDrawer} 
+                    closed={this.sideDrawerClosedHandler} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </>
+        )
+    }
+} 
+
+export default Layout;
